6996cc0cd1ee0452fb76ac94a230a7f056e54299
Isha <67051067+ishapatell1@users.noreply.github.com>
2025-01-23 12:37:38 +0530

    add : router

--- blinkit/src/App.jsx
+++ blinkit/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {Routes, Route} from "react-router-dom"
 import './App.css'
+import { Home } from './pages/Home'
 
 function App() {
  
   return (
     <>
       <h1>Blinkit Clone App</h1>
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+      </Routes>
     </>
   )
 }
